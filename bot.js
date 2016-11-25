0708e3b5820f50b6bb0169d765fce883d3c634cb
timinoc <timinoc@gmail.com>
2016-11-25 14:10:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : "https://www.youtube.com/watch?v=i13CX-NXdL8"
+    "text" : "https://www.youtube.com/watch?v=JxmkVyMPEkg"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
